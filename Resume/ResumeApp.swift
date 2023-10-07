cef087c0eddda3598095fd186ccd011b10cd41cb
Everton Buzzi <evertonbuzzi@gmail.com>
2023-10-07 11:28:01 +0100

    Initial Commit

---
+++ Resume/ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by Everton Buzzi on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
